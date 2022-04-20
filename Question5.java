0279b0d5762bb9828f39a021e804e98d625a682f
kc-np <103633515+kc-np@users.noreply.github.com>
2022-04-20 10:23:13 +0800

    answer question 5

--- Question5.java
+++ Question5.java
@@ -27,6 +27,38 @@ public class Question5
      */
      
     Scanner in = new Scanner(System.in);
-    
+
+    System.out.print("Enter a number: ");
+    int num = in.nextInt();
+
+    int[] numbers = new int[num];
+
+    for (int a = 0; a < num; a++) {
+        System.out.print("> ");
+        int number = in.nextInt();
+        numbers[a] = number;
+    }
+
+    int[] mode = {0, 0};  // {frequency, number}
+
+    for (int a = 0; a < num; a++) {
+        int frequency = 0;
+        int number = numbers[a];
+
+        for (int b = a + 1; b < num; b++) {
+            int number2 = numbers[b];
+            if (number == number2) {
+                frequency += 1;
+            }
+        }
+
+        if (frequency > mode[0]) {
+            mode[0] = frequency;
+            mode[1] = number;
+        }
+    }
+
+    System.out.println(mode[1]);
+  
   }
 }
