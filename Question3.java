70330b4afff0786cad63b4d2376befe424298c7a
kc-np <103633515+kc-np@users.noreply.github.com>
2022-04-20 10:20:51 +0800

    answer question 3

--- Question3.java
+++ Question3.java
@@ -11,6 +11,11 @@ public class Question3
      */
      
     Scanner in = new Scanner(System.in);
-    
+
+    System.out.print("Enter a number: ");
+    int num = in.nextInt();
+    int result = num * num;
+
+    System.out.println(result);
   }
 }
