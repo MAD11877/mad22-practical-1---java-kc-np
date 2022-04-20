0905a2af7257940851c1b8f1e97528740f73ff66
kc-np <103633515+kc-np@users.noreply.github.com>
2022-04-20 10:20:14 +0800

    answer question 2

--- Question2.java
+++ Question2.java
@@ -11,6 +11,15 @@ public class Question2
      */
      
     Scanner in = new Scanner(System.in);
+
+    System.out.print("Enter height: ");
     double height = in.nextDouble();
+
+    System.out.print("Enter weight: ");
+    double weight = in.nextDouble();
+
+    double bmi = weight / (height * height);
+
+    System.out.println("Your bmi is " + bmi);
   }
 }
